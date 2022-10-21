7d25bb402b517928f0161338803662d64ef2d9b5
tamaki03 <datthanhdoan.charlie@gmail.com>
2022-10-21 07:49:52 +0700

    bai th so 5

--- tuan5-1.sql
+++ tuan5-1.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS orders (
   orderNumber int NOT NULL AUTO_INCREMENT,
-  orderdate DATETIME ,
+  orderDate DATETIME ,
   requiredDate DATETIME,
   shippedDate DATETIME ,
   status varchar(15) NOT NULL,	
@@ -27,9 +27,9 @@ CREATE TABLE IF NOT EXISTS products (
   quantityInStock smallint NOT NULL,
   buyPrice double NOT NULL,
   PRIMARY KEY (productCode),
-  FOREIGN KEY orderdetails(productLine) REFERENCES productlines(productLine) ON UPDATE CASCADE 
+  FOREIGN KEY (productLine) REFERENCES productlines(productLine) ON UPDATE CASCADE 
 );
-
+ALTER TABLE products ADD MSRP double NOT NULL;
 CREATE TABLE IF NOT EXISTS orderdetails (
   orderNumber int NOT NULL AUTO_INCREMENT,
   productCode varchar(15) NOT NULL,
@@ -90,19 +90,8 @@ CREATE TABLE IF NOT EXISTS offices(
     territory varchar(100) NOT NULL
 );
 
-ALTER TABLE products ADD MSRP double NOT NULL;
 
-INSERT INTO customers
-	(customerNumber,customerName,contactLastName,contactFirstName,phone,addressLine1,addressLine2,city,state,postalCode,country,salesRepEmployeeNumber,creditLimit)
-VALUES
-	(103,	'Atelier graphique',	'Schmitt',	'Carine', 	'40.32.2555',	'54, rue Royale',	NULL,	'Nantes',	NULL,	'44000',	'France',	1370,	21000),
-	(112,	'Signal Gift Stores',	'King',	'Jean',	'7025551838',	'8489 Strong St.',	NULL,	'Las Vegas',	'NV',	'83030',	'USA',	1166,	71800),
-	(114,	'Australian Collectors Co.',	'Ferguson',	'Peter',	'03 9520 4555',	'636 St Kilda Road',	'Level 3',	'Melbourne',	'Victoria',	'3004',	'Australia',	1611,	117300),
-	(119,	'La Rochelle Gifts',	'Labrune',	'Janine', 	'40.67.8555',	'67, rue des Cinquante Otages',	NULL,	'Nantes',	NULL,	'44000',	'France',1370,	118200),
-	(121,	'Baane Mini Imports',	'Bergulfsen',	'Jonas', 	'07-98 9555',	'Erling Skakkes gate 78',	NULL,	'Stavern',	NULL,	'4110',	'Norway',1504,	81700),
-	(124,	'Mini Gifts Distributors Ltd.',	'Nelson',	'Susan',	'4155551450',	'5677 Strong St.',	NULL,	'San Rafael',	'CA',	'97562','USA',	1165,	210500),
-	(125,	'Havel & Zbyszek Co',	'Piestrzeniewicz',	'Zbyszek', 	'(26) 642-7555',	'ul. Filtrowa 68',	NULL,	'Warszawa',	NULL,	'01-012',	'Poland',	NULL,	0),
-	(146,	'Saveley & Henriot, Co.',	'Saveley',	'Mary', 	'78.32.5555',	'2, rue du Commerce',	NULL,	'Lyon',	NULL,	'69004',	'France',	1337,	123900);
+
 
 
 INSERT INTO orders
@@ -121,19 +110,6 @@ VALUES
 	(10394,'2005-03-15','2005-03-25','2005-03-19','Shipped',NULL,141);
 
 
-INSERT INTO employees
-	(employeeNumber,lastName,firstName,extension,email,officeCode,reportsTo,jobTitle)
-VALUES
-	(1002,	'Murphy',	'Diane',	'x5800',	'dmurphy@classicmodelcars.com',	1,	NULL,	'President'),
-	(1056,	'Patterson',	'Mary',	'x4611',	'mpatterso@classicmodelcars.com',	1,	1002,	'VP Sales'),
-	(1076,	'Firrelli',	'Jeff',	'x9273',	'jfirrelli@classicmodelcars.com',	1,	1002,	'VP Marketing'),
-	(1088,	'Patterson',	'William',	'x4871',	'wpatterson@classicmodelcars.com',	6,	1056,	'Sales Manager (APAC)'),
-	(1102,	'Bondur',	'Gerard',	'x5408',	'gbondur@classicmodelcars.com',	4,	1056,	'Sale Manager (EMEA)'),
-	(1143,	'Bow',	'Anthony',	'x5428',	'abow@classicmodelcars.com',	1,	1056,	'Sales Manager (NA)'),
-	(1165,	'Jennings',	'Leslie', 'x3291','ljennings@classicmodelcars.com',	1,	1143,	'Sales Rep'),
-	(1166,	'Thompson',	'Leslie',	'x4065',	'lthompson@classicmodelcars.com',	1,	1143,	'Sales Rep'),
-	(1188,	'Firrelli',	'Julie',	'x2173', 'jfirrelli@classicmodelcars.com',	2,	1143,	'Sales Rep'),
-	(1216,	'Patterson',	'Steve',	'x4334',	'spatterson@classicmodelcars.com',	2,	1143,	'Sales Rep');
     
 INSERT INTO productlines
 	(productLine,	textDescription,	htmlDescription,	image)
@@ -147,6 +123,7 @@ VALUES
 	('Vintage Cars',	'Our Vintage Car models realistically portray automobiles produced from the early 1900s through the 1940s. Materials used include Bakelite, diecast, plastic and wood. Most of the replicas are in the 1:18 and 1:24 scale sizes, which provide the optimum in detail and accuracy. Prices range from $30.00 up to $180.00 for some special limited edition replicas. All models include a certificate of authenticity from their manufacturers and come fully assembled and ready for display in the home or office.',	NULL,	NULL);
 
 
+
 INSERT INTO products
 	(productCode,productName,productLine,productScale,productVendor,productDescription,quantityInStock,buyPrice,MSRP)
 VALUES
@@ -157,6 +134,37 @@ VALUES
 	('S18_1097',	'1940 Ford Pickup Truck',	'Trucks and Buses',	'1:18',	'Studio M Art Models',	'This model features soft rubber tires, working steering, rubber mud guards, authentic Ford logos, detailed undercarriage, opening doors and hood,  removable split rear gate, full size spare mounted in bed, detailed interior with opening glove box',	2613,	58.33,	116.67),
 	('S18_1129',	'1993 Mazda RX-7',	'Classic Cars',	'1:18',	'Highway 66 Mini Classics',	'This model features, opening hood, opening doors, detailed engine, rear spoiler, opening trunk, working steering, tinted windows, baked enamel finish. Color red.',	3975,	83.51,	141.54),
 	('S18_1342',	'1937 Lincoln Berline',	'Vintage Cars',	'1:18',	'Motor City Art Classics',	'Features opening engine cover, doors, trunk, and fuel filler cap. Color black',	8693,	60.62,	102.74);
+
+
+
+INSERT INTO customers
+	(customerNumber,customerName,contactLastName,contactFirstName,phone,addressLine1,addressLine2,city,state,postalCode,country,salesRepEmployeeNumber,creditLimit)
+VALUES
+	(103,	'Atelier graphique',	'Schmitt',	'Carine', 	'40.32.2555',	'54, rue Royale',	NULL,	'Nantes',	NULL,	'44000',	'France',	1370,	21000),
+	(112,	'Signal Gift Stores',	'King',	'Jean',	'7025551838',	'8489 Strong St.',	NULL,	'Las Vegas',	'NV',	'83030',	'USA',	1166,	71800),
+	(114,	'Australian Collectors Co.',	'Ferguson',	'Peter',	'03 9520 4555',	'636 St Kilda Road',	'Level 3',	'Melbourne',	'Victoria',	'3004',	'Australia',	1611,	117300),
+	(119,	'La Rochelle Gifts',	'Labrune',	'Janine', 	'40.67.8555',	'67, rue des Cinquante Otages',	NULL,	'Nantes',	NULL,	'44000',	'France',1370,	118200),
+	(121,	'Baane Mini Imports',	'Bergulfsen',	'Jonas', 	'07-98 9555',	'Erling Skakkes gate 78',	NULL,	'Stavern',	NULL,	'4110',	'Norway',1504,	81700),
+	(124,	'Mini Gifts Distributors Ltd.',	'Nelson',	'Susan',	'4155551450',	'5677 Strong St.',	NULL,	'San Rafael',	'CA',	'97562','USA',	1165,	210500),
+	(125,	'Havel & Zbyszek Co',	'Piestrzeniewicz',	'Zbyszek', 	'(26) 642-7555',	'ul. Filtrowa 68',	NULL,	'Warszawa',	NULL,	'01-012',	'Poland',	NULL,	0),
+	(146,	'Saveley & Henriot, Co.',	'Saveley',	'Mary', 	'78.32.5555',	'2, rue du Commerce',	NULL,	'Lyon',	NULL,	'69004',	'France',	1337,	123900);
+
+
+
+
+INSERT INTO employees
+	(employeeNumber,lastName,firstName,extension,email,officeCode,reportsTo,jobTitle)
+VALUES
+	(1002,	'Murphy',	'Diane',	'x5800',	'dmurphy@classicmodelcars.com',	1,	NULL,	'President'),
+	(1056,	'Patterson',	'Mary',	'x4611',	'mpatterso@classicmodelcars.com',	1,	1002,	'VP Sales'),
+	(1076,	'Firrelli',	'Jeff',	'x9273',	'jfirrelli@classicmodelcars.com',	1,	1002,	'VP Marketing'),
+	(1088,	'Patterson',	'William',	'x4871',	'wpatterson@classicmodelcars.com',	6,	1056,	'Sales Manager (APAC)'),
+	(1102,	'Bondur',	'Gerard',	'x5408',	'gbondur@classicmodelcars.com',	4,	1056,	'Sale Manager (EMEA)'),
+	(1143,	'Bow',	'Anthony',	'x5428',	'abow@classicmodelcars.com',	1,	1056,	'Sales Manager (NA)'),
+	(1165,	'Jennings',	'Leslie', 'x3291','ljennings@classicmodelcars.com',	1,	1143,	'Sales Rep'),
+	(1166,	'Thompson',	'Leslie',	'x4065',	'lthompson@classicmodelcars.com',	1,	1143,	'Sales Rep'),
+	(1188,	'Firrelli',	'Julie',	'x2173', 'jfirrelli@classicmodelcars.com',	2,	1143,	'Sales Rep'),
+	(1216,	'Patterson',	'Steve',	'x4334',	'spatterson@classicmodelcars.com',	2,	1143,	'Sales Rep');
 
 INSERT INTO payments
 	(customerNumber,	checkNumber,	paymentDate,	amount)
@@ -182,29 +190,35 @@ VALUES
 	(6,	'Sydney',	'+61 2 9264 2451',	'5-11 Wentworth Avenue',	'Floor #2',	NULL,	'Australia',	'NSW 2010',	'APAC'),
 	(7,	'London',	'+44 20 7877 2041',	'25 Old Broad Street',	'Level 7',	NULL,	'UK',	'EC2N 1HN',	'EMEA');
 
--- cau 1 --
-SELECT reportsTo,lastName,firstName
-FROM employees
-WHERE reportsTo IS NULL;
+-- bai 1
 
--- cau2 --
-SELECT *
-FROM orders
-WHERE orderDate is NOT NULL;
+select substring(productDescription,1,50) as "Title of products"
+from products;
 
--- cau 3 --
-SELECT *
-FROM orders 
-WHERE requiredDate = '2003-01-18';
 
--- cau 4 --
-SELECT orderNumber, orderDate,status
-FROM orders	
-WHERE status = 'Shipped' AND orderdate between '2005-04-01' AND '2005-04-30';
-	
--- cau 5 -- 
+-- bai 2
 
-SELECT productName,productLine
-FROM products
-WHERE productLine='Classic Cars';
+SELECT CONCAT(firstName," ",lastName) as "Full Name", jobTitle
+from employees;
+
+-- bai 3
+
+UPDATE products
+Set productLine = REPLACE(productLine,"Cars","Automobiles");
+
+-- bai 4
+
+SELECT DATEDIFF(requiredDate,shippedDate) as "days"
+from orders
+WHERE shippedDate is NOT NULL
+order BY days ASC
+LIMIT 5;
+
+
+-- bai 5
+
+SELECT orderDate,shippedDate
+from orders
+WHERE shippedDate is NULL AND orderDate between "2005-05-01" AND "2005-05-31"
+
 
